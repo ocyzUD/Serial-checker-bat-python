f1e861ad24eef5c74f9c71cdc52bb439363e3b23
ocyzUD <ocyz.lua@outlook.com>
2025-03-05 17:28:57 -0800

    Create serial checker.bat

---
+++ serial checker.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Hardware Information:
+echo.
+
+echo CPU Serial Number:
+wmic cpu get ProcessorId
+echo.
+
+echo GPU Serial Number:
+wmic path win32_videocontroller get PNPDeviceID
+echo.
+
+echo RAM Serial Number:
+wmic memorychip get SerialNumber
+echo.
+
+echo Disk Serial Number:
+wmic diskdrive get SerialNumber
+echo.
+
+echo Motherboard Information
+eho Motherboard Serial Number:
+wmic baseboard get SerialNumber
+echo.
+
+echo HWID (UUID):
+wmic csproduct get UUID
+echo.
+
+pause
